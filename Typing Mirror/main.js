fac95d1502f7249949e7ce109a11d79ef6ce1f31
ThotaMounika <thotasrinagamounika@gmail.com>
2018-07-05 23:42:49 +0530

    Second project

--- Typing Mirror/main.js
+++ Typing Mirror/main.js
@@ -2,3 +2,29 @@ function printFunction(){
 var data=document.getElementById('box').value;
 document.getElementById('demo').innerHTML=data;
 }
+
+function uppercase(){
+	var text=document.getElementById("box").value;
+	if(text!=""){
+	text=text.toUpperCase();
+	document.getElementById("demo").innerHTML=text;
+}
+else{
+	alert("Enter Something")
+}
+}
+
+
+function lowercase(){
+	var text=document.getElementById("box").value;
+	if(text!=""){
+	text=text.toLowerCase();
+	document.getElementById("demo").innerHTML=text;
+}
+else{
+	alert("Enter Some Text..")
+}
+}
+
+
+
